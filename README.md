0de2d80a44e339a284dd91ca535bf111527a69d4
vince13011 <vincent.giglio@hotmail.fr>
2021-02-04 18:52:41 +0100

    Mise en place Express et dépendances

---
+++ README.md
@@ -0,0 +1,15 @@
+# Portfolio jeux de société
+
+<description>
+
+## Stack
+
+- Node 10+
+  - Express
+  - Joi
+- PostgreSQL 11+
+  - Sqitch
+
+## Structure des données
+
+<mcd>
---
+++ index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3698;
+
+const apiRouter = require('./app/router');
+
+app.use('/v1', apiRouter);
+
+app.listen(port, () => console.log(`Server listening on http://localhost:${port}`));
