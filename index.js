bd495bbfdec4b3da6f9c98b1aa8eb9cac7771dfa
vincent giglio <vincent.giglio@hotmail.fr>
2021-02-25 20:52:50 +0100

    Ajout de  commentaires  sur les fichiers principaux

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
+/*
+ dotenv est un module sans dépendance qui charge les variables d'environnement à partir 
+d'un fichier .env dans process.env.
+*/
 require('dotenv').config();
 
 const express = require('express');
 
 const app = express();
 
-const port = process.env.PORT || 3698;
+// Si nous n'avons pas créer de fichier .env le port par défaut sera le port 4000
+const port = process.env.PORT || 4000;
 
 
 const apiRouter = require('./app/router');
 
-
+//express.json est un bodyParser qui retourne un middleware qui analyse du json
 app.use(express.json())
 
+// on dit à express d'utiliser notre router avec un sufix v1 pour préciser la  version de notre API
 app.use('/v1', apiRouter);
 
 
